fc38a1eeb26aa439cd16c5d388e6ee3010c81c69
vikrant julme <162186341+VickyJulme@users.noreply.github.com>
2024-08-12 16:43:25 +0530

    first commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DataScience from './assets/DataScience/DataHome'
+const App = () => {
+  return (
+    <div>
+
+      <DataScience/>
+    </div>
+  )
+}
+
+export default App
